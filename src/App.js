6c5a882fe6a824e8b474e70896a75cf9e21c292b
haidarfahmii <59414625+haidarfahmii@users.noreply.github.com>
2020-12-20 09:39:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import GlobalStyle from './globalStyles';
-import { Navbar } from './components';
-import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Home from './pages/HomePage/Home';
-import Service from './pages/SignUp/Service/Service';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import { Navbar, Footer } from './components';
 import LoginApp from './pages/SignUp/LoginApp';
 
 function App() {
   return (
     <Router>
-      <GlobalStyle /> 
+      <GlobalStyle />
+      <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/SignUp" exact component = {LoginApp} />
+        <Route path='/' exact component={Home} />
+        <Route path='/SignUp' exact component={LoginApp} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubText,
+  FooterSubHeading,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
+} from './Footer.elements';
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name='email' type='email' placeholder='Your Email' />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to='/SignUp'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to='/'>Contact</FooterLink>
+            <FooterLink to='/'>Support</FooterLink>
+            <FooterLink to='/'>Destinations</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to='/'>
+            <SocialIcon />
+            FATBURN
+          </SocialLogo>
+          <WebsiteRights>FATBURN © 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink 
+                href={'//www.instagram.com/haidarfahmii/'} 
+                target='_blank' 
+                aria-label='Instagram'>
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink 
+                href= {'//www.linkedin.com/in/haidar-fahmi-2bb09b200/'} 
+                target='_blank' 
+                aria-label='LinkedIn'>
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
